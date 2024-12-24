fa913dbbf575825d0ef49af57029ed7720f61be7
safirmdhn <safiramadhani9@gmail.com>
2024-12-24 12:28:42 +0800

    Update dependencies and add notification proto definitions

--- domain/notification.go
+++ domain/notification.go
@@ -1,9 +1,9 @@
 package domain
 
 type Notification struct {
-	ID        string `json:"id"`
-	Subject   string `json:"subject"`
-	Body      string `json:"body"`
+	ID      string `json:"id"`
+	Subject string `json:"subject"`
+	// Body      string `json:"body"`
 	UserEmail string `json:"user_id"`
 	OrderID   int    `json:"order_id"`
 	PaymentID int    `json:"payment_id"`
@@ -14,27 +14,27 @@ type Notification struct {
 func NotificationData() []Notification {
 	return []Notification{
 		{
-			ID:        "1",
-			Subject:   "Order Confirmation",
-			Body:      "Your order has been confirmed",
+			ID:      "1",
+			Subject: "Order Confirmation",
+			// Body:      "Your order has been confirmed",
 			UserEmail: "user1@example.com",
 			OrderID:   1,
 			Type:      "order_notification",
 			IsSend:    false,
 		},
 		{
-			ID:        "2",
-			Subject:   "Payment Confirmation",
-			Body:      "Your payment has been confirmed",
+			ID:      "2",
+			Subject: "Payment Confirmation",
+			// Body:      "Your payment has been confirmed",
 			UserEmail: "user2@example.com",
 			PaymentID: 1,
 			Type:      "payment_notification",
 			IsSend:    false,
 		},
 		{
-			ID:        "3",
-			Subject:   "Order Confirmation",
-			Body:      "Your order has been confirmed",
+			ID:      "3",
+			Subject: "Order Confirmation",
+			// Body:      "Your order has been confirmed",
 			UserEmail: "user3@example.com",
 			OrderID:   2,
 			Type:      "order_notification",
